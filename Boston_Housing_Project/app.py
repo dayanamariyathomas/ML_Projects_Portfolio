22863a4cfc9e8edb41e9a3247beedd709027be1d
Dayana Maria <dayanamariyathomas759@gmail.com>
2023-05-30 19:12:49 +0530

    Modified the Model

--- Boston_Housing_Project/app.py
+++ Boston_Housing_Project/app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify, render_template
 import numpy as np
 
 app = Flask(__name__)
-regmodel = pickle.load(open('regmodel.pkl', 'rb'))
-scalar = pickle.load(open('scaling.pkl', 'rb'))
+regmodel = pickle.load(open('model.pkl', 'rb'))
+scaler = pickle.load(open('minmax_scaler.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,14 +13,16 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = {
-        'CRIM': float(request.form['CRIM']),
-        'ZN': float(request.form['ZN']),
         'INDUS': float(request.form['INDUS']),
         'RM': float(request.form['RM']),
         'PTRATIO': float(request.form['PTRATIO']),
-        'LSTAT': float(request.form['LSTAT'])
+        'LSTAT': float(request.form['LSTAT']),
+        'NOX': float(request.form['NOX']),
+        'TAX': float(request.form['TAX']),
+        'AGE': float(request.form['AGE']),
+        'DIS': float(request.form['DIS']),
     }
-    final_input = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    final_input = scaler.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(final_input)
     return render_template('home.html', prediction_text="The House Price Prediction is ${:.2f}".format(output[0]))
 
--- Boston_Housing_Project/model.py
+++ Boston_Housing_Project/model.py
@@ -9,27 +9,23 @@ df = pd.read_csv("Housingdata.csv")
 
 # Check for missing values
 print(df.isnull().sum())
-# replace null values with median value for each column
+# replace null values with mean value for each column
 df = df.fillna(df.mean())
 
 # Scaling
 min_max_scaler = MinMaxScaler()
-column_selected = ['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
+column_selected = ['INDUS','NOX','PTRATIO','RM','TAX','AGE','LSTAT','DIS']
 x = df.loc[:, column_selected]
 y = df['MEDV']
 x_scaled = min_max_scaler.fit_transform(x)
 x = pd.DataFrame(x_scaled, columns=column_selected)
 
-# Splitting the dependent and independent variables
-X = df[['CRIM','INDUS','RAD','NOX','PTRATIO','RM','TAX','B','AGE','LSTAT','DIS']]
-y = df.iloc[:, -1]
-
 # Splitting the dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Training the Random Forest model
 rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
-rf_regressor.fit(X_train, y_train)
+rf_regressor.fit(x_train, y_train)
 
 # Pickling the MinMaxScaler
 with open('minmax_scaler.pkl', 'wb') as f:
